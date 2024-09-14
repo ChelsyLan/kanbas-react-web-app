1a13199e0496cd1035da1fe0e0ba315294202697
chengxulan <lanchengxu0807@gmail.com>
2024-09-13 19:13:57 -0700

    add name and github link

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,11 +4,13 @@ import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 
+// Name: Chengxu Lan
 export default function Labs() {
     return (
       <div>
         <h1>Labs</h1>
         <TOC />
+        <a href="https://github.com/ChelsyLan/kanbas-react-web-app" id="wd-github"></a>
         <Routes>
           <Route path="/" element={<Navigate to="Lab1" />} />
           <Route path="Lab1" element={<Lab1 />} />
